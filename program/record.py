cf07574574ba3c72a2a369a71895811c2361e79e
Glek2 <99312596+Glek2@users.noreply.github.com>
2024-03-30 11:59:31 +0300

    Create record.py

---
+++ program/record.py
@@ -0,0 +1,25 @@
+class NewClass:
+  return
+
+
+class Record:
+  def __init__(self):
+    #Иницилазиция класса записи
+
+  def AddNewRecord(self):
+    #Модуль добавления новой записи
+
+  def RedactRecord(self):
+    #Модуль изменения записи
+
+  def DeleteRecord(self):
+    #Модуль удаления записи
+
+  def SearchRecord(self):
+    #Модуль поиска по записям
+
+  def ImportRecord(self):
+    #Модуль импорта записей из файла
+
+  def ExproetRecord(self):
+    #Модуль экспорта записей в файл
