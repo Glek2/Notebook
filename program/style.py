e678845d094d23d4cbf79b6866e1344491153f48
Glek2 <99312596+Glek2@users.noreply.github.com>
2024-03-30 11:32:04 +0300

    Update style.py

--- program/style.py
+++ program/style.py
@@ -1,1 +1,21 @@
+class Style:
+  def __init__(self):
+    #Иницилацзия класса настройки и оформления
 
+  def Sinhronazation(self):
+    #Синхронизация с облачным хранилищем
+    
+  def Setting(self):
+    #Настройка тем оформления
+    
+  def SettingText(self):
+    #Настройка шрифтов и размера текста
+    
+  def CreateTeg(self):
+    #Создание категорий и тегов для записей
+    
+  def FiltrRecord(self):
+    #Фильтрация записей по категориям и тегам
+    
+  def AddVideoAndPicture(self):
+    #Добавление изображений и видео к записям
