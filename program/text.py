ae0ab090e49ebaba6d73a79ed8896b91b152e6de
Glek2 <99312596+Glek2@users.noreply.github.com>
2024-03-30 11:49:51 +0300

    Update text.py

--- program/text.py
+++ program/text.py
@@ -1,1 +1,18 @@
+class Text:
+  def __init__(self):
+    #Инициализация класса текста
 
+  def AutomaticlyRecover(self):
+    #Автоматическое исправление орфографии
+    
+  def Voiceover(self):
+    #Озвучивание текста записей
+    
+  def TranslateText(self):
+    #Перевод текста на другие языки
+    
+  def CreateWithVoice(self):
+    #Создание заметок с распознаванием голоса
+    
+  def MultilingualSupport(self):
+    #Поддержка различных языков интерфейса
