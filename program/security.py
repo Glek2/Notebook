65f6942296dfff52d5a02907d5cd7c088023cf85
Glek2 <99312596+Glek2@users.noreply.github.com>
2024-03-30 12:08:58 +0300

    Revert "Delete program/security.py"

---
+++ program/security.py
@@ -0,0 +1,18 @@
+class Security:
+  def __init__(self):
+    #Иницилазация класса безопасности
+
+  def Shifr(self):
+    #Шифрование и защита паролем
+
+  def Statics(self):
+    #Статистика использования блокнота
+
+  def Integration(self):
+    #Интеграция с календарём
+
+  def Razdelenie(self):
+    #Разделение записей на группы
+
+  def AddSsilki(self):
+    #Добавление ссылок и веб-страниц к записям
