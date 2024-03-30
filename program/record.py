0a8b25f4da4c17201a1345905f7ae27899c1f476
Glek2 <99312596+Glek2@users.noreply.github.com>
2024-03-30 10:34:14 +0300

    Update record.py

--- program/record.py
+++ program/record.py
@@ -1,1 +1,12 @@
+class Record:
+  def __init__(self):
+    #Иницилазиция класса записи
 
+  def AddNewRecord(self):
+    #Модуль добавления новой записи
+
+  def RedactRecord(self):
+    #Модуль изменения записи
+
+  def DeleteRecord(self):
+    #Модуль удаления записи
