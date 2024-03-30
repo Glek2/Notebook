211afaafb95afee0fe87d3220136f4f54633905d
Glek2 <99312596+Glek2@users.noreply.github.com>
2024-03-30 11:42:44 +0300

    Update management.py

--- program/management.py
+++ program/management.py
@@ -1,1 +1,18 @@
+class Managment:
+  def __init__(self):
+    #Инициализация класса управления
 
+  def CreateTask(self):
+    #Создание задач и напоминаний
+
+  def GetPrioritet(self):
+    #Установка приоритета для записей
+
+  def SendOnPrint(self):
+    #Отправка записей на печать
+
+  def AddAudio(self):
+    #Добавление аудиозаписей к записям
+
+  def Archivation(self):
+    #Архивирование и восстановление записей
